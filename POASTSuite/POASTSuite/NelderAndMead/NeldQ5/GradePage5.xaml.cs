18786e2bbfa8407cdd888c0063238170e0d7f56e
tubobelem1@outlook.com <tubobelem1@outlook.com>
2021-11-10 08:15:43 +0100

    added question five

---
+++ POASTSuite/POASTSuite/NelderAndMead/NeldQ5/GradePage5.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace POASTSuite.NelderAndMead.NeldQ5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GradePage5 : ContentPage
+    {
+        private double score;
+        public GradePage5(double T2)
+        {
+            InitializeComponent();
+            score = T2;
+        }
+
+        private async void BtnNxt4_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new PoastMainPage());
+        }
+
+        private void BtnSolution_Clicked(object sender, EventArgs e)
+        {
+            if (score == 100)
+            {
+                quote.Text = "EXCELLENT!";
+            }
+            else if (score > 70 && score <= 99)
+            {
+                quote.Text = "VERY GOOD";
+            }
+            else if (score < 70 && score >= 50)
+            {
+                quote.Text = "GOOD";
+            }
+            else
+            {
+                quote.Text = "YOU CAN DO BETTER!";
+            }
+
+            Score.Text = score + "%".ToString();
+        }
+
+        private async void SelectionPage5_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new SolutionPage5());
+        }
+    }
+}
---
+++ POASTSuite/POASTSuite/NelderAndMead/NeldQ5/SolutionPage5.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace POASTSuite.NelderAndMead.NeldQ5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SolutionPage5 : ContentPage
+    {
+        public SolutionPage5()
+        {
+            InitializeComponent();
+        }
+    }
+}
